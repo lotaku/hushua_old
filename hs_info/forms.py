7c1afd94aed22b2636be1f5f66d6ebe5fc045db6
loatku <317399510@qq.com>
2014-08-13 23:39:18 +0800

    不在models里面添加verbose参数，通过form 类表单处理

--- hs_info/forms.py
+++ hs_info/forms.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
 from django import forms
+from django.utils.translation import ugettext as _
 from django.forms.extras.widgets import SelectDateWidget
 from hs_info.models import HsInfo
 
@@ -12,13 +13,18 @@ REPUTATION = (('1心','1心'),
 	('钻号','钻号')
 	)
 
+
 class HsInfoForm(forms.ModelForm):
-	# birth_year = forms.DateField(widget=SelectDateWidget(years=BIRTH_YEAR_CHOICES))
-	# favorite_colors = forms.MultipleChoiceField(required=False,
-	# 	widget=forms.CheckboxSelectMultiple, choices=FAVORITE_COLORS_CHOICES)
-	reputation_gt = forms.MultipleChoiceField(required=False,
-		widget=forms.CheckboxSelectMultiple, choices=REPUTATION)
+	def __init__(self, *args, **kwargs):
+		super(HsInfoForm, self).__init__(*args, **kwargs)
+		self.fields['reputation_gt'].label = "信誉大于"
+		self.fields['shop_type'].label = "店铺类型"
+		self.fields['sign_required'].label = "是否签收"
+		
+		# self.fields['new_password'].label = _("New Password")
+		# self.fields['new_password_again'].label = _("Confirm Password")
 
 	class Meta:
-	        model = HsInfo
-	        # fields = ('user','week_limited')
+		model = HsInfo
+		# fields = ('user','week_limited', 'reputation_gt', 'shop_type')
+		# self.fields = fields	
