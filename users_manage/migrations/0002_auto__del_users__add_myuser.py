d66b016dc174f38a963acf8630ef9685e40e0fb9
loatku <317399510@qq.com>
2014-08-17 18:34:39 +0800

    新增文件

---
+++ users_manage/migrations/0002_auto__del_users__add_myuser.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting model 'Users'
+        db.delete_table(u'users_manage_users')
+
+        # Adding model 'MyUser'
+        db.create_table(u'users_manage_myuser', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('password', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('last_login', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('email', self.gf('django.db.models.fields.EmailField')(unique=True, max_length=255)),
+            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('is_admin', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal(u'users_manage', ['MyUser'])
+
+
+    def backwards(self, orm):
+        # Adding model 'Users'
+        db.create_table(u'users_manage_users', (
+            ('password', self.gf('django.db.models.fields.CharField')(max_length=8, null=True, blank=True)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=100, null=True, blank=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        ))
+        db.send_create_signal(u'users_manage', ['Users'])
+
+        # Deleting model 'MyUser'
+        db.delete_table(u'users_manage_myuser')
+
+
+    models = {
+        u'users_manage.myuser': {
+            'Meta': {'object_name': 'MyUser'},
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_admin': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        }
+    }
+
+    complete_apps = ['users_manage']
