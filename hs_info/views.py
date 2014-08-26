db08f637afca42241fd3540780deac5afa5b85e4
loatku <317399510@qq.com>
2014-08-26 23:12:12 +0800

    改进分页，修改展示页面

--- hs_info/views.py
+++ hs_info/views.py
@@ -8,45 +8,39 @@ from hs_info.models import HsInfo
 
 
 def index(request):
-	page = 1
-	# page = request.GET.get('page', 1)
+	
+	page = int(request.GET.get('page', 1))
 
 	paginate_by = 1
 	task_list = HsInfo.objects.all()
 	paginator = Paginator(task_list, paginate_by)
 	page_obj = paginator.page(page)
 
-	# print page_obj
-	print "^&"*45
-	# print dir(Paginator.num_pages)
-	# previous_list
-	# for i in range(0, page_obj.page_)
-
 	num_pages = paginator.num_pages
 	pagination_list =[]
 	pagination_list.append(page)
+	
+	if num_pages <=10:
+		print num_pages
+		pagination_list = [ item for item in range(1,num_pages+1)]
 
-	i = 0
-	for _ in range(0,page):
-		i += 1 
-		page_num = page-i
-		if page_num >0:
-			pagination_list.insert(0,page_num)
-		else:
-			break
-		if i==5:
-			break
-	i = 0
-	for _ in range(page,num_pages):
-		i += 1 
-		page_num = page+i
-		if page_num <num_pages:
-			pagination_list.append(page_num)
-		else:
-			break
-		if i==5:
-			break
-	print pagination_list
+	elif page<10:
+		pagination_list = [ item for item in range(1,11)]
+
+	else:
+		list_right=[]
+
+		for idx ,item in enumerate(range(page, num_pages+1)):
+			list_right.append(item)
+			if idx==5:
+				break
+		left_len = 10 - len(list_right)
+		print left_len
+		left_len = 0 if left_len < 0 else left_len
+		list_left = [item for item in range(page-left_len, page)]
+		pagination_list = list_left + list_right
+		print pagination_list
+
 	context= {
 			'page_obj':page_obj,
 			'paginator': paginator,
